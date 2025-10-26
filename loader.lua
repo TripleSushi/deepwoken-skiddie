f882285ce3f0e0f62e19b48e056e44de6c998cc5
TripleSushi <csgovalve1222@gmail.com>
2025-10-25 23:21:36 -0600

    Added main.lua to the loader

--- loader.lua
+++ loader.lua
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 local Repo = "https://raw.githubusercontent.com/TripleSushi/deepwoken-skiddie/main"
 
 local Client = loadstring(game:HttpGet(Repo .. "/modules/client.lua"))()
 local KeyHandler = loadstring(game:HttpGet(Repo .. "/modules/kh.lua"))()
 local Main = loadstring(game:HttpGet(Repo .. "/main.lua"))()
-=======
-local Repo = "https://github.com/TripleSushi/deepwoken-skiddie"
-
-local Client = loadstring(game:HttpGet(Repo .. "/modules/client.lua"))()
-local KeyHandler = loadstring(game:HttpGet(Repo .. "/modules/kh.lua"))()
->>>>>>> 7689d71e60f7eac7f79e7989a73e3199145eb8a6
 
 Client.init()
 KeyHandler.init()
@@ -21,10 +14,6 @@ end
 return {
     Client = Client,
     KeyHandler = KeyHandler,
-<<<<<<< HEAD
     GetKey = getKey,
     Main = Main
-=======
-    GetKey = getKey
->>>>>>> 7689d71e60f7eac7f79e7989a73e3199145eb8a6
 }
--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local Main = {}
 
 local playersService = game:GetService("Players")
@@ -11,6 +10,3 @@ end
 
 
 return Main
-=======
--- random
->>>>>>> 7689d71e60f7eac7f79e7989a73e3199145eb8a6
