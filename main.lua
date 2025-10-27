8e7349025eda8cb906c9794e34f9957f6d3a3ac4
TripleSushi <csgovalve1222@gmail.com>
2025-10-26 20:51:06 -0600

    Removed unnecessary return

--- main.lua
+++ main.lua
@@ -5,14 +5,14 @@ local Main = {}
 
 local playersService = game:GetService("Players")
 local LocalPlayer = playersService.LocalPlayer
-
+local Enmity = loadstring(game:HttpGet('https://raw.githubusercontent.com/TripleSushi/deepwoken-skiddie/main/features/enmity.lua'))()
 
 --Prevent execution in unwanted places
 --Prevents execution in Deepwoken Menu by checking the PlaceId
 if game.PlaceId == 4111023553 then
     LocalPlayer:Kick("Only run in main-game")
-    return Main
 end
 
+Enmity.listener()
 
 return Main
