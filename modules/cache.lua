77ccf94a47523b39a2aca368fd67fb8cbb5c3b14
TripleSushi <csgovalve1222@gmail.com>
2025-10-30 20:14:57 -0600

    Fixed cache storage

--- modules/cache.lua
+++ modules/cache.lua
@@ -1,24 +1,27 @@
 getgenv().Cache = getgenv().Cache or {}
-
 local Repo = "https://raw.githubusercontent.com/TripleSushi/deepwoken-skiddie/main"
 
 local function load(path)
     local url = Repo .. path
 
-    if getgenv().Cache[url] then
-        return getgenv().Cache[url]
+    if not getgenv().Cache[url] then
+        local success, func = pcall(function()
+            return loadstring(game:HttpGet(url))
+        end)
+
+        if not success then
+            warn("Failed to load: ", path, func)
+            return nil
+        end
+
+        getgenv().Cache[url] = func
     end
 
-    local success, module = pcall(function()
-        return loadstring(game:HttpGet(url))()
-    end)
-
+    local success, module = pcall(getgenv().Cache[url])
     if not success then
-        warn("Failed to load: ", path, module)
+        warn("Failed to execute: ", path, module)
         return nil
     end
-
-    getgenv().Cache[url] = module
 
     return module
 end
