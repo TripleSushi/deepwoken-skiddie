a643175afe05bda160afe2a46ae9cae15db262fe
TripleSushi <csgovalve1222@gmail.com>
2025-10-31 18:51:59 -0600

    Added wait time

--- features/autobank.lua
+++ features/autobank.lua
@@ -25,7 +25,7 @@ local function depositItem(itemName)
         if not item then break end
 
         event:FireServer("deposit", item)
-
+        wait(0.5)
         local prompt = gui:WaitForChild("ChoicePrompt", 2)
         if prompt then
             local choice = prompt:FindFirstChild("Choice")
